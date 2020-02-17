5a8aa782dbb35ef0cc2ca4590213c8fd61bab2f3
Anmol Bajaj <hello@anmolbajaj.net>
2020-02-17 15:47:35 -0800

    Add hospital distance

--- ass2-explor.py
+++ ass2-explor.py
@@ -13,13 +13,34 @@ station = pd.read_csv("nyc-transit-data.csv")
 station = station.filter(['Station Name', 'Station Latitude', 'Station Longitude'])
 station.drop_duplicates(inplace=True)
 
+# Clean up code for hospital data
+point_of_intrest = pd.read_csv("Point_Of_Interest.csv")
+point_of_intrest =  point_of_intrest[point_of_intrest['NAME'].str.contains("HOSPITAL")]
+point_of_intrest['the_geom'] = point_of_intrest['the_geom'].str.split(' ')
+point_of_intrest['latitude'] = point_of_intrest['the_geom'].apply(lambda x: x[1])
+point_of_intrest['longitude'] = point_of_intrest['the_geom'].apply(lambda x: x[0])
+# print(point_of_intrest['latitude'])
+# print(point_of_intrest['longitude'])
+
+# Get distance to hospitals
+
+mat_hospital = scipy.spatial.distance.cdist(df[['latitude','longitude']], 
+                              point_of_intrest[['latitude','longitude']], metric='euclidean')
+
+min_distance_to_hospital = []
+for listing in mat_hospital:
+    min_distance_to_hospital.append(min(listing))
+
+df['closest_hospital'] = min_distance_to_hospital
+
+# Get distance to subway stations
 mat = scipy.spatial.distance.cdist(df[['latitude','longitude']], 
                               station[['Station Latitude','Station Longitude']], metric='euclidean')
 
 
-print(len(mat[0]))
-print(len(mat))
-print(min(mat[0]))
+# print(len(mat[0]))
+# print(len(mat))
+# print(min(mat[0]))
 
 min_distance_to_station = []
 
@@ -28,5 +49,5 @@ for listing in mat:
 
 # print(min_distance_to_station)
 
-df['distance_to_closest'] = min_distance_to_station
-# print(df)
+df['closest_station'] = min_distance_to_station
+print(df)
